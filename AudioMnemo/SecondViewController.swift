72abc2eeaab8cad3a48aee905f075e1dc70ea412
yong gu <yonggu@cernet2.net>
2015-10-08 13:58:00 +0800

    Initial Commit

---
+++ AudioMnemo/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  AudioMnemo
+//
+//  Created by yong gu on 10/8/15.
+//  Copyright © 2015 Thu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
